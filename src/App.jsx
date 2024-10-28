195df15a94514cbbd4154b7e0ad64b694436e7cc
Ravi Pratihast <ravipratihast@gmail.com>
2024-10-28 17:49:15 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import { Home, Email, Dashboard } from "./pages/index-page";
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/email" element={<Email />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/index-page.js
@@ -0,0 +1,5 @@
+import { Dashboard } from "./DashboardPage/Dashboard";
+import { Email } from "./EmailPage/Email";
+import { Home } from "./HomePage/Home";
+
+export { Dashboard, Email, Home };
