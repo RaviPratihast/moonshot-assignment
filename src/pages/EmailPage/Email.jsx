e32e7fb26f3aa64e3ca793284ccb8e5c78ad46af
Ravi Pratihast <ravipratihast@gmail.com>
2024-10-28 22:32:09 +0530

    Test: testing  email list component.

--- src/pages/EmailPage/Email.jsx
+++ src/pages/EmailPage/Email.jsx
@@ -1,5 +1,11 @@
+import { EmailCardComponent } from "../../Component/EmailComponents/EmailCardComponent";
 const Email = () => {
-  return <div>Email</div>;
+  return (
+    <div>
+      Email
+      <EmailCardComponent />
+    </div>
+  );
 };
 
 export { Email };
