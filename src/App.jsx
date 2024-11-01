7fddfedb16a7a62dd0cf0c85c38e6ea044cd23c1
Ravi Pratihast <ravipratihast@gmail.com>
2024-11-01 13:53:37 +0530

    Refactor: removal of unwanted code.

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/email" element={<Email />} />
-        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
       </Routes>
     </>
   );
--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -5,19 +5,13 @@ const Home = () => {
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="text-center p-8 rounded-lg shadow-lg bg-white max-w-md w-full">
         <h1 className="text-3xl font-bold mb-6 text-gray-800">
-          Choose Your Assignment
+          Moonshot Assignment
         </h1>
-        <p className="text-gray-600 mb-8">Select the assignment</p>
+        {/* <p className="text-gray-600 mb-8">Select the assignment</p> */}
         <div className="flex flex-col gap-4">
           <Link to="/email">
-            <button className="w-full py-3 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 transition duration-300 ease-in-out">
-              Email Assignment
-            </button>
-          </Link>
-
-          <Link to="/dashboard">
-            <button className="w-full py-3 px-4 bg-green-500 text-white font-semibold rounded-lg shadow-md hover:bg-green-600 transition duration-300 ease-in-out">
-              Dashboard Assignment
+            <button className="w-full py-3 px-4 bg-accent text-white font-semibold rounded-lg shadow-md border hover:border-accent hover:text-text hover:bg-white transition duration-300 ease-in-out">
+              Moonshot Email Assignment
             </button>
           </Link>
         </div>
